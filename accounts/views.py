1759aaeea3198aa27c505e43be43daa8c42372a0
peterdinis <petter1@pobox.sk>
2024-12-31 22:57:38 +0100

    infra: Start working on auth endpoints WIP

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from reset_framework import status
+from django.contrib.auth.hashers import make_password
+from .serializers import SignUpSerializers, UserSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+@api_view(["POST"])
+def register_user(request):
+    pass
