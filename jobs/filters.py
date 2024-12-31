603351235149e0a5d865d007e183448f95713a46
peterdinis <petter1@pobox.sk>
2024-12-31 22:14:56 +0100

    fix: Fixing issue in filters.py

--- jobs/filters.py
+++ jobs/filters.py
@@ -4,10 +4,13 @@ from .models import Job
 class JobFilter(filters.FilterSet):
 
     keyword = filters.CharFilter(field_name="title", lookup_expr="icontains")
-    location = filters.CharFilter(filed_name="address", lookup_expr="icontains")
-    min_salary = filters.NumberFilter(field_name="salary" or 0, lookup_expr="gte")
-    max_salary = filters.NumberFilter(filed_name="salary" or 100000, lookup_expr="lte")
+    location = filters.CharFilter(field_name="address", lookup_expr="icontains")
+    min_salary = filters.NumberFilter(field_name="salary", lookup_expr="gte")
+    max_salary = filters.NumberFilter(field_name="salary", lookup_expr="lte")
+    education = filters.ChoiceFilter(field_name="education", choices=Job.Education.choices)
+    job_type = filters.ChoiceFilter(field_name="job_type", choices=Job.JobType.choices)
+    experience = filters.ChoiceFilter(field_name="experience", choices=Job.Experience.choices)
 
     class Meta:
         model = Job
-        fields = ("keyword", "location", "education", "jobType", "expirience", "min_salary", "max_salary")
+        fields = ("keyword", "location", "education", "job_type", "experience", "min_salary", "max_salary")
