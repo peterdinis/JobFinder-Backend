21672983fa2b0abc9808bf60e183093d725d50a2
peterdinis <petter1@pobox.sk>
2025-01-01 01:11:07 +0100

    infra: Writing auth logic

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("register/", views.register_user, name="register_user")
+    path("register/", views.register_user, name="register_user"),
+    path("me/", views.currentUser, name="current_user")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth.hashers import make_password
 from .serializers import SignUpSerializers, UserSerializer
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(["POST"])
 def register_user(request):
@@ -34,3 +35,14 @@ def register_user(request):
         return Response(response_serializer.data, status=status.HTTP_201_CREATED)
     
     return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def currentUser(request):
+    """
+    Retrieve the current authenticated user's information.
+    """
+    user = request.user  # Get the authenticated user
+    serializer = UserSerializer(user)  # Serialize the user object
+    return Response(serializer.data, status=status.HTTP_200_OK)
