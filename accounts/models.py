cec8a9883c8f8c7bc3d3ce932b61858a498364f8
peterdinis <petter1@pobox.sk>
2025-01-01 15:20:49 +0100

    infra: Start to updating user profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name="userprofile", on_delete=models.CASCADE)
     resume = models.FileField(upload_to='resumes/', blank=True, null=True)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    pass
     
